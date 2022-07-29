f2d7a18b7c1a0f2f604aa1cafdec41593dad11fc
philip-morlier <philipmorlier@gmail.com>
2022-07-28 17:56:43 -0700

    Further development of polygon plugin

--- plugins/packages/polygon/main.go
+++ plugins/packages/polygon/main.go
@@ -28,15 +28,9 @@ type cardinalBorReceiptMeta struct {
 	LogOffset         uint
 }
 
-// receiptRegexp = regexp.MustCompile("c/[0-9a-z]+/b/([0-9a-z]+)/r/([0-9a-z]+)")
-// c/89/b/xxx(bk hash)/br/yy(tx index)
+borReceiptRegexp = regexp.MustCompile("c/[0-9a-z]+/b/([0-9a-z]+)/br/([0-9a-z]+)")
 
-borReceiptRegexp = regexp..MustCompile("c/89+/b/([0-9a-z]+)/br/([0-9a-z]+)")
-
-// logRegexp = regexp.MustCompile("c/[0-9a-z]+/b/([0-9a-z]+)/l/([0-9a-z]+)/([0-9a-z]+)")
-// c/89/b/xxx/bl/yy/zz
-
-borLogRegexp = regexp.MustCompile("c/89/b/([0-9a-z]+)/bl/([0-9a-z]+)/([0-9a-z]+)")
+borLogRegexp = regexp.MustCompile("c/[0-9a-z]+/b/([0-9a-z]+)/bl/([0-9a-z]+)/([0-9a-z]+)")
 
 func getTopicIndex(topics []common.Hash, idx int) []byte {
 	if len(topics) > idx {
@@ -53,12 +47,13 @@ func Indexer(cfg config.Config) indexer.Indexer {
 	return &PolygonIndexer{Chainid: cfg.Chainid}
 }
 
-func (pg *PolygonIndexer) index(pb *delivery.PendingBatch) ([]string, error) {
+func (pg *PolygonIndexer) Index(pb *delivery.PendingBatch) ([]string, error) {
 
-	blockNumber := pb.Number
-	blockHash := pb.Hash
+	encNum := make([]byte, 8)
+	binary.BigEndian.PutUint64(encNum, pb.Number)
+	txHash := crypto.Keccak256(append(append([]byte("-matic-bor-receipt-"), encNum...), pb.Hash...))
 
-	receiptData := make(map[int]*cardinalBorReceiptMeta)
+	receiptData := make(map[int][]byte)
 	logData := make(map[int64]*gtypes.Log)
 
 	for k, v := range pb.Values {
@@ -66,9 +61,7 @@ func (pg *PolygonIndexer) index(pb *delivery.PendingBatch) ([]string, error) {
 		case borReceiptRegexp.MatchString(k):
 			parts := receiptRegexp.FindSubmatch([]byte(k))
 			txIndex, _ := strconv.ParseInt(string(parts[2]), 16, 64)
-			rmeta := &cardinalReceiptMeta{}
-			rlp.DecodeBytes(v, rmeta)
-			receiptData[int(txIndex)] = rmeta
+			receiptData[int(txIndex)] = v
 		
 		case borLogRegexp.MatchString(k):
 			parts := borLogRegexp.FindSubmatch([]byte(k))
@@ -87,71 +80,64 @@ func (pg *PolygonIndexer) index(pb *delivery.PendingBatch) ([]string, error) {
 	statements := []string{ //we need to either make applyParameters public or recreate in this package
 		applyParameters("DELETE FROM bor_receipts WHERE number >= %v", pb.Number),
 	}
+	for txIndex, logsBloom := range receiptData { 
+		statements = append(statements, applyParameters(
+			"INSERT INTO bor_receipts(hash, transactionIndex, number) VALUES (%v, %v, %v)",
+			txHash,
+			txIndex,
+			compress(logsBloom),
+			pb.Number,
+		))}
 	statements = append(statements, applyParameters(
-		"INSERT INTO bor_receipts(hash, transactionIndex, number) VALUES (%v, %v, %v)",
-		pb.Hash,
-		txIndex,
-		getCopy(compress(receipt.LogsBloom)),
-		pb.Number,
-	))
-	statements = append(statements, applyParameters(
-		"DELETE FROM bor_logs WHERE blockHash >= %v", pb.Hash),
+	"DELETE FROM bor_logs WHERE blockHash >= %v", pb.Number),
 	)
-	statements = append(statements, applyParameters(
-		"INSERT INTO bor_logs(address, topic0, topic1, topic2, topic3, topic4, data, blockHash, block) VALUES (%v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v)",
-		logRecord.Address,
-		getTopicIndex(logRecord.Topics, 0),
-		getTopicIndex(logRecord.Topics, 1),
-		getTopicIndex(logRecord.Topics, 2),
-		getTopicIndex(logRecord.Topics, 3),
-		getTopicIndex(logRecord.Topics, 4), //this is an assumption that needs to be checked
-		compress(logRecord.Data),
-		// bortxhash Keccak256("matic-bor-receipt-",binencode(number),blockhash)
-		crypto.Keccak256("matic-bor-receipt" + binary.BigEndian.PutUint64(pb.Number) + pb.Hash))
-		logRecord.TxIndex,
-		pb.Hash,
-		pb.Number,
-		logRecord.Index,
-	)
+	for logIndex, logRecord := range logData {
+		statements = append(statements, applyParameters(
+			"INSERT INTO bor_logs(address, topic0, topic1, topic2, topic3, data, transactionHash, transactionIndex, blockHash, block, logIndex) VALUES (%v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v)",
+			logRecord.Address,
+			getTopicIndex(logRecord.Topics, 0),
+			getTopicIndex(logRecord.Topics, 1),
+			getTopicIndex(logRecord.Topics, 2),
+			getTopicIndex(logRecord.Topics, 3),
+			compress(logRecord.Data),
+			txHash,
+			logRecord.TxIndex,
+			pb.Hash,
+			pb.Number,
+			logIndex,
+		))}
 	return statements, nil
+	}
 }
 
 
 func Migrate(db *sql.DB, chainid uint64) error {
 	var tableName string
-	db.QueryRow("SELECT name FROM bor_receipts WHERE type='table' and name='migrations';").Scan(&tableName);
+	db.QueryRow("SELECT name FROM bor.sqlite_master WHERE type='table' and name='migrations';").Scan(&tableName);
 	if tableName != "migrations" {
-		db.Exec("CREATE TABLE blocks.migrations (version integer PRIMARY KEY);")
+		db.Exec("CREATE TABLE bor.migrations (version integer PRIMARY KEY);")
 
-		db.Exec("INSERT INTO blocks.migrations(version) VALUES (0);")
+		db.Exec("INSERT INTO bor.migrations(version) VALUES (0);")
 	}
 	var schemaVersion uint
-	db.QueryRow("SELECT version FROM bor_receipts.migrations;").Scan(&schemaVersion)
+	db.QueryRow("SELECT version FROM bor.migrations;").Scan(&schemaVersion)
 	if schemaVersion < 1 {
-		if _, err := db.Exec(`CREATE TABLE bor_receipts (
+		if _, err := db.Exec(`CREATE TABLE bor.bor_receipts (
 			id INTEGER PRIMARY KEY AUTOINCREMENT,
 			hash varchar(32) UNIQUE,
 			transactionIndex MEDIUMINT,
 			logsBloom blob,
-			status TINYINT, // we actually dont need this, should be removed at migration, we should check once we have the old db live to connect to 
 			block BIGINT
 	        );`); err != nil { return err }
-		}
-	db.QueryRow("SELECT name FROM bor_logs WHERE type='table' and name='migrations';").Scan(&tableName);
-	if tableName != "migrations" {
-		db.Exec("CREATE TABLE bor_logs.migrations (version integer PRIMARY KEY);")
+		
+		if _, err := db.Exec(`CREATE INDEX bor.receiptBlock ON bor_receipts(block)`); err != nil { log.Error("bor_receiptBlock CREATE INDEX error", "err", err.Error()) }
 
-		db.Exec("INSERT INTO blocks.migrations(version) VALUES (0);")
-	}
-	db.QueryRow("SELECT version FROM bor_receipts.migrations;").Scan(&schemaVersion)
-	if schemaVersion < 1 {
-		if _, err := db.Exec(`CREATE TABLE bor_logs (
+		if _, err := db.Exec(`CREATE TABLE bor.bor_logs (
 			address varchar(20),
 			topic0 varchar(32),
 			topic1 varchar(32),
 			topic2 varchar(32),
 			topic3 varchar(32),
-			topic4 varchar(32),
 			data blob,
 			transactionHash varchar(32),
 			transactionIndex varcahr(32),
@@ -160,8 +146,8 @@ func Migrate(db *sql.DB, chainid uint64) error {
 			logIndex MEDIUMINT,
 			PRIMARY KEY (block, logIndex)
 			);`); err != nil { return err }
-		}
-
+	
+	}
 	return nil
 }
 
