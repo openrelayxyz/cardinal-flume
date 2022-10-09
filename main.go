3e3e172066e8952d86bab89837606a0ad8eb1635
philip-morlier <philipmorlier@gmail.com>
2022-10-09 11:23:06 -0700

    plugin loader changed to load plugins from cfg

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"strings"
-	"path/filepath"
+	// "strings"
+	// "path/filepath"
 	"context"
 	"database/sql"
 	"flag"
 	"fmt"
-	"golang.org/x/exp/slices"
+	// "golang.org/x/exp/slices"
 	log "github.com/inconshreveable/log15"
 	"github.com/mattn/go-sqlite3"
 	rpcTransports "github.com/openrelayxyz/cardinal-rpc/transports"
@@ -38,29 +38,12 @@ func main() {
 		os.Exit(1)
 	}
 
-	pluginsPath := cfg.PluginDir
-	pl, err := plugins.NewPluginLoader(pluginsPath)
+	pl, err := plugins.NewPluginLoader(cfg)
 	if err != nil {
 		log.Error("No PluginLoader initialized", "err", err.Error())
-	}
+ 	}
 
 	pl.Initialize(cfg)
-
-	var pluginNames []string
-	for _, plugin := range pl.Plugins {
-		name := strings.Split(filepath.Base(plugin.Name), ".")[0]
-		pluginNames = append(pluginNames, name)
-	}
-
-	for _, plugin := range cfg.Plugins {
-		if !slices.Contains(pluginNames, plugin) {
-			log.Error("required plugin not present", "plugin", plugin)
-			return
-		}
-	}
-
-	log.Debug("all required plugins loaded", "plugins", pluginNames)
-
 
 	sql.Register("sqlite3_hooked",
 		&sqlite3.SQLiteDriver{
--- plugins/plugin_loader.go
+++ plugins/plugin_loader.go
@@ -1,10 +1,10 @@
 package plugins
 
 import (
-	"io/ioutil"
+	// "io/ioutil"
 	"path"
 	"plugin"
-	"strings"
+	// "strings"
 
 	log "github.com/inconshreveable/log15"
 	"github.com/openrelayxyz/flume/config"
@@ -38,30 +38,26 @@ func (pl *PluginLoader) Lookup(name string, validate func(interface{}) bool) []i
 	return results
 }
 
-func NewPluginLoader(target string) (*PluginLoader, error) {
+func NewPluginLoader(cfg *config.Config) (*PluginLoader, error) {
 	pl := &PluginLoader{
 		Plugins:     []pluginDetails{},
 		LookupCache: make(map[string][]interface{}),
 	}
-	files, err := ioutil.ReadDir(target)
-	if err != nil {
-		log.Warn("Could not load plugins directory. Skipping.", "path", target)
-		return pl, nil
-	}
-	for _, file := range files {
-		fpath := path.Join(target, file.Name())
-		if !strings.HasSuffix(file.Name(), ".so") {
-			log.Debug("File inplugin directory is not '.so' file. Skipping.", "file", fpath)
-			continue
-		}
+
+	pluginDirectory := cfg.PluginDir
+
+	for _, name := range cfg.Plugins {
+		file := name + ".so"
+		fpath := path.Join(pluginDirectory, file)
 		plug, err := plugin.Open(fpath)
 		if err != nil {
-			log.Warn("File in plugin directory could not be loaded: %v", "file", fpath, "error", err.Error())
-			continue
+			log.Error("File in plugin directory could not be loaded: %v", "file", fpath, "error", err.Error())
+			panic(err)
 		}
 
 		pl.Plugins = append(pl.Plugins, pluginDetails{plug, fpath})
 	}
+	log.Info("all required plugins loaded", "plugins", cfg.Plugins)
 	return pl, nil
 }
 
