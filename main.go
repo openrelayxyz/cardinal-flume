c2853fa816f78772fc3c29288bc7c0beeb376f13
philip-morlier <philipmorlier@gmail.com>
2022-12-05 15:06:45 -0800

    Added pragma page_size parameter to main.go. Pushing directly to develop.

--- main.go
+++ main.go
@@ -48,7 +48,7 @@ func main() {
 		&sqlite3.SQLiteDriver{
 			ConnectHook: func(conn *sqlite3.SQLiteConn) error {
 				for name, path := range cfg.Databases {
-					conn.Exec(fmt.Sprintf("ATTACH DATABASE '%v' AS '%v'; PRAGMA %v.journal_mode = WAL ; PRAGMA %v.synchronous = OFF ; pragma %v.max_page_count = 2147483646 ;", path, name, name, name, name), nil)
+					conn.Exec(fmt.Sprintf("ATTACH DATABASE '%v' AS '%v'; PRAGMA %v.page_size = 65536 ; PRAGMA %v.journal_mode = WAL ; PRAGMA %v.synchronous = OFF ; pragma %v.max_page_count = 2147483646 ;", path, name, name, name, name, name), nil)
 				}
 				return nil
 			},
