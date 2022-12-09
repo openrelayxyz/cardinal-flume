927a3de11ba784380006b73daff635eacada88db
philip-morlier <philipmorlier@gmail.com>
2022-12-09 10:38:07 -0800

    Added json unmarshalling to DecimalOrHex type

--- api/types.go
+++ api/types.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"math/big"
 	"encoding/json"
+	"strings"
+	"strconv"
 
 	log "github.com/inconshreveable/log15"
 	"github.com/openrelayxyz/cardinal-evm/common"
@@ -14,6 +16,23 @@ import (
 )
 
 type DecimalOrHex uint64
+
+func (dh *DecimalOrHex) UnmarshalJSON(data []byte) error {
+	input := strings.TrimSpace(string(data))
+	if len(input) >= 2 && input[0] == '"' && input[len(input)-1] == '"' {
+		input = input[1 : len(input)-1]
+	}
+
+	value, err := strconv.ParseUint(input, 10, 64)
+	if err != nil {
+		value, err = hexutil.DecodeUint64(input)
+	}
+	if err != nil {
+		return err
+	}
+	*dh = DecimalOrHex(value)
+	return nil
+}
 
 type bigList []*big.Int
 
