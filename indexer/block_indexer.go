e6535aea6313d92fbd615d951fb778ec07d804e7
philip-morlier <philipmorlier@gmail.com>
2023-04-13 11:04:23 -0700

    Added blockTimer variable to indexer
    A new metric blockAgeTimer was also added which collects the blockTimer values. Also if the value is > 1 minute we add the "age" to the logs.

--- indexer/block_indexer.go
+++ indexer/block_indexer.go
@@ -3,6 +3,7 @@ package indexer
 import (
 	"encoding/binary"
 	"fmt"
+	"time"
 
 	log "github.com/inconshreveable/log15"
 	"github.com/openrelayxyz/cardinal-evm/crypto"
@@ -70,6 +71,7 @@ func (indexer *BlockIndexer) Index(pb *delivery.PendingBatch) ([]string, error) 
 	if err := rlp.DecodeBytes(headerBytes, &header); err != nil {
 		return nil, err
 	}
+	*blockTime = time.Unix(int64(header.Time), 0)
 
 	eblock := &extblock{
 		Header: header,
