5bdd5d0fb1762193f55f19a9170f4001b7483a1e
philip-morlier <philipmorlier@gmail.com>
2023-06-13 07:24:06 -0700

    Compound address_topic0 index created.
    The same index was added to the polygon migrations table.
    
    There is some pork here. There was some general cleanup of the migrations as well as a missing argument placed in deliver consumer call in aquire consumer.

--- migrations/migrations.go
+++ migrations/migrations.go
@@ -296,11 +296,11 @@ func MigrateLogs(db *sql.DB, chainid uint64) error {
 		log.Info("logs migrations v2 done")
 	}
 	if schemaVersion < 3 {
-		if _, err := db.Exec(`CREATE INDEX logs.addressCompound ON event_logs(address, topic0, block);`); err != nil {
-			log.Error("Migrate Logs CREATE INDEX logs.addressCompound error", "err", err.Error())
+		if _, err := db.Exec(`CREATE INDEX logs.address_topic0_compound ON event_logs(address, topic0, block);`); err != nil {
+			log.Error("Migrate Logs CREATE INDEX logs.addresstopic0_compound error", "err", err.Error())
 			return nil
 		}
-		db.Exec(`UPDATE logs.migrations SET version = 2;`)
+		db.Exec(`UPDATE logs.migrations SET version = 3;`)
 		log.Info("logs migrations v3 done")
 	}
 
