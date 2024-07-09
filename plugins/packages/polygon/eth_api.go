19976de6d26d75e86b0a2846dbd6f4343a2b15d7
Philip Morlier <philipmorlier@gmail.com>
2024-07-09 16:34:09 -0700

    Modifications to getTransactionReceiptsByBlock api and test function and control data.

--- plugins/packages/polygon/eth_api.go
+++ plugins/packages/polygon/eth_api.go
@@ -352,14 +352,13 @@ func (service *PolygonEthService) GetTransactionReceiptsByBlock(ctx context.Cont
 	if err != nil {
 		return nil, errBlockNotFound
 	}
-
 	if borTxHashBytes != nil {
 		var nilMap map[string]interface{}
-		borReceipt, _ := GetTransactionReceipt(nilMap, plugins.BytesToHash(borTxHashBytes), service.db)
-		receipts = append(receipts, borReceipt)
-		for _, receipt := range receipts {
-			receipt["transactionHash"] = plugins.BytesToHash(borTxHashBytes)
+		borReceipt, err := GetTransactionReceipt(nilMap, plugins.BytesToHash(borTxHashBytes), service.db)
+		if err != nil {
+			log.Error("error aquiring bor receipt", "err", err)
 		}
+		receipts = append(receipts, borReceipt)
 	}
 
 	for _, receipt := range receipts {
