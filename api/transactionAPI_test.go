1ca0f0bbd4ec9c0afdc7921530b7edea8991300e
Philip Morlier <philipmorlier@gmail.com>
2025-04-01 20:45:41 -0700

    updated api test blocks database to include blobGasUsed and blobGasPrice on block 19576279.
    This will enable the receipt api utility function to accurately calculate blobGasPrice and blobGasUsed

--- api/transactionAPI_test.go
+++ api/transactionAPI_test.go
@@ -141,7 +141,7 @@ func TestTransactionAPI(t *testing.T) {
 
 			actualReceipt := *actual;
 			for key, expectedValue := range expectedReceipt {
-				if key == "blobGasPrice" || key == "root" {
+				if key == "root" {
 					continue
 				} else {
 					actualValue := actualReceipt[key]
