e8c5b048509dc1f6e9bea39dc496f31171a73dcf
Philip Morlier <philipmorlier@gmail.com>
2025-04-28 10:59:05 -0700

    removed dependence on previous checking functions

--- api/utils.go
+++ api/utils.go
@@ -106,20 +106,19 @@ func previousBlockPresent(input interface{}, cfg *config.Config, db *sql.DB, isT
 	var present bool
 	var response int
 	if isTx {
-		if txDataPresent((input.(types.Hash)), cfg, db, false) {
-			txStatement := "SELECT block FROM transactions.transactions WHERE hash = ?;"
-			db.QueryRow(txStatement, trimPrefix(input.(types.Hash).Bytes())).Scan(&response)
-			if uint64(response -1) <= cfg.EarliestBlock {
-				present = true
-			}
-		} 
+		txStatement := "SELECT block FROM transactions.transactions WHERE hash = ?;"
+		db.QueryRow(txStatement, trimPrefix(input.(types.Hash).Bytes())).Scan(&response)
+		if uint64(response -1) <= cfg.EarliestBlock {
+			present = true
+		}
 	} else {
-		if blockDataPresent(input, cfg, db) {
-			statement := "SELECT number FROM blocks.blocks WHERE hash = ?;"
-			db.QueryRow(statement, trimPrefix(input.(types.Hash).Bytes())).Scan(&response)
-			if uint64(response -1) <= cfg.EarliestBlock {
-				present = true
-			}
+		if w := cfg.Waiter; w != nil {
+			w.WaitForHash(input.(types.Hash), cfg.WaitTime)
+		}
+		statement := "SELECT number FROM blocks.blocks WHERE hash = ?;"
+		db.QueryRow(statement, trimPrefix(input.(types.Hash).Bytes())).Scan(&response)
+		if uint64(response -1) <= cfg.EarliestBlock {
+			present = true
 		}
 	}
 	return present
