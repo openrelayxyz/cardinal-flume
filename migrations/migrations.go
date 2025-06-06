da326feeebc450eb2a7519643219eb82ded75882
Philip Morlier <philipmorlier@gmail.com>
2025-06-06 15:30:52 -0700

    typos in migration

--- migrations/migrations.go
+++ migrations/migrations.go
@@ -246,28 +246,28 @@ func MigrateBlocks(db *sql.DB, chainid uint64) error {
 		switch chainid {
 		case 137:
 			if _, err := db.Exec(fmt.Sprintf(`INSERT INTO blocks.baseFeeDenominatorSchedule(startBlock, endBlock, denominator) VALUES (%v, %v, %v)`, 0, 38189055, 8)); err != nil {
-				log.Error("migrations hoodi INSERT INTO blocks.baseFeeDenominatorSchedule v1 error", "err", err.Error())
+				log.Error("migrations hoodi INSERT INTO blocks.baseFeeDenominatorSchedule error", "err", err.Error())
 				return nil
 			}
 			if _, err := db.Exec(fmt.Sprintf(`INSERT INTO blocks.baseFeeDenominatorSchedule(startBlock, endBlock, denominator) VALUES (%v, %v, %v)`, 38189056, maxInt, 16)); err != nil {
-				log.Error("migrations hoodi INSERT INTO blocks.baseFeeDenominatorSchedule v1 error", "err", err.Error())
+				log.Error("migrations hoodi INSERT INTO blocks.baseFeeDenominatorSchedule error", "err", err.Error())
 				return nil
 			}
 			if _, err := db.Exec(fmt.Sprintf(`INSERT INTO blocks.baseFeeDenominatorSchedule(startBlock, endBlock, denominator) VALUES (%v, %v, %v)`, maxInt, maxInt, 64)); err != nil {
-				log.Error("migrations hoodi INSERT INTO blocks.baseFeeDenominatorSchedule v1 error", "err", err.Error())
+				log.Error("migrations hoodi INSERT INTO blocks.baseFeeDenominatorSchedule error", "err", err.Error())
 				return nil
 			}
 		default:
 			if _, err := db.Exec(fmt.Sprintf(`INSERT INTO blocks.baseFeeDenominatorSchedule(startBlock, endBlock, denominator) VALUES (%v, %v, %v)`, 0, maxInt, 8)); err != nil {
-				log.Error("migrations default INSERT INTO blocks.baseFeeDenominatorSchedule v1 error", "err", err.Error())
+				log.Error("migrations default INSERT INTO blocks.baseFeeDenominatorSchedule error", "err", err.Error())
 				return nil
 			}
 		}
-		if _, err := db.Exec("UPDATE blocks.migrations SET version = 7;"); err != nil {
-			log.Error("migrations UPDATE blocks.migrations v7 error", "err", err.Error())
+		if _, err := db.Exec("UPDATE blocks.migrations SET version = 8;"); err != nil {
+			log.Error("migrations UPDATE blocks.migrations v8 error", "err", err.Error())
 			return nil
 		}
-		log.Info("blocks v7 migrations done")
+		log.Info("blocks v8 migrations done")
 	}
 
 	log.Info("blocks migration up to date")
